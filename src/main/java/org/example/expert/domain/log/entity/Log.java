76cf549824d8526c6bb9bebf137493a1a595b9a6
youngrae0317 <isp1229@naver.com>
2025-09-19 17:45:57 +0900

    Lv3-2: Transaction 심화 기본 틀 구현 (Log 도메인)

---
+++ src/main/java/org/example/expert/domain/log/entity/Log.java
@@ -0,0 +1,29 @@
+package org.example.expert.domain.log.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.expert.domain.common.entity.Timestamped;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "logs")
+public class Log extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String requestContents;
+
+    public Log(String requestContents) {
+        this.requestContents = requestContents;
+    }
+}
---
+++ src/main/java/org/example/expert/domain/log/service/LogService.java
@@ -0,0 +1,22 @@
+package org.example.expert.domain.log.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.log.entity.Log;
+import org.example.expert.domain.log.repository.LogRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class LogService {
+
+    private final LogRepository logRepository;
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void saveLog(String contents) {
+
+        Log log = new Log(contents);
+        logRepository.save(log);
+    }
+}
